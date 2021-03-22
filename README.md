19bc8eae9a2de71c2ca9a7d4d30fe58610408418
carlosmartinezt <carlosmartinezt@outlook.com>
2021-03-22 09:25:36 -0400

    migrated identity server to sql but Google stopped working

--- README.md
+++ README.md
@@ -2,4 +2,14 @@ Set UserSecretsId to '2cefd9c5-852a-4e28-b804-81f37d8e2299' for MSBuild project 
 
 dotnet user-secrets set ConnectionStrings:Lists "Data Source=77.75.120.158; Database=Lists; User Id=carlos; password=juliovoltio"
 
+Connection string: 
+Data Source=77.75.120.158; Database=Lists; User Id=carlos; password=juliovoltio
+
 dotnet ef dbcontext scaffold Name=ConnectionStrings:Lists Microsoft.EntityFrameworkCore.SqlServer --context-dir Data --output-dir Data/Models
+
+
+Continue reading: 
+- https://docs.microsoft.com/en-us/aspnet/core/security/authentication/scaffold-identity?view=aspnetcore-5.0&tabs=netcore-cli
+- https://docs.microsoft.com/en-us/ef/core/managing-schemas/scaffolding?tabs=dotnet-core-cli
+- https://docs.identityserver.io/en/latest/quickstarts/6_aspnet_identity.html
+
--- list-of-lists-identity-server/Startup.cs
+++ list-of-lists-identity-server/Startup.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-
 using IdentityServer4;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace list_of_lists_identity_server {
     public class Startup {
@@ -21,22 +21,22 @@ namespace list_of_lists_identity_server {
         public void ConfigureServices(IServiceCollection services) {
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            const string connectionString = @"Data Source=77.75.120.158; Database=Lists; User Id=carlos; password=juliovoltio";
 
-            var builder = services.AddIdentityServer(options => {
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-                options.EmitStaticAudienceClaim = true;
-            })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
-                .AddTestUsers(TestUsers.Users);
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
+            services.AddIdentityServer()
+                .AddTestUsers(TestUsers.Users)
+                .AddConfigurationStore(options => {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options => {
+                    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                }).AddDeveloperSigningCredential();
 
             services.AddAuthentication()
-                .AddGoogle("Google", options =>
-                {
+                .AddGoogle("Google", options => {
                     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
 
                     options.ClientId = "908238563051-0oj3lg64luh53bh9mn6jibd8suefu10i.apps.googleusercontent.com";
