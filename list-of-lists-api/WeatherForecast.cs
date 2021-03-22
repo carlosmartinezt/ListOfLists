30a1395c66a545e048f06601b902d2dc7457249f
carlosmartinezt <carlosmartinezt@outlook.com>
2021-03-21 20:58:41 -0400

    Added identity server

---
+++ list-of-lists-api/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace list_of_lists_api
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
